c044dcb4059f7e43457a9f89f1c99d86bc640f9d
jongmin1217 <syj408886@gmail.com>
2021-07-12 18:45:18 +0900

    test

--- app/src/main/java/com/example/usefulmemo/di/MyApplication.kt
+++ app/src/main/java/com/example/usefulmemo/di/MyApplication.kt
@@ -1,18 +1,11 @@
 package com.example.usefulmemo.di
 
 import android.app.Application
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 
 class MyApplication : Application(){
 
     override fun onCreate() {
         super.onCreate()
 
-        startKoin{
-            androidLogger()
-            androidContext(this@MyApplication)
-        }
     }
 }
